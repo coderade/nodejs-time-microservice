bb21c915f4b3bc5e43d5cafb5712ea96487a953f
Valdeci Gomes <coderade@outlook.com>
2017-11-19 17:55:02 -0200

    Implements geocoding service based on the GOOGLE Geocode API

--- bin/run.js
+++ bin/run.js
@@ -4,7 +4,7 @@ const service = require('../server/service');
 const http = require('http');
 
 const server = http.createServer(service);
-server.listen(3000);
+server.listen(3010);
 
 server.on('listening', function() {
     console.log(`The time micro-service is listening on ${server.address().port} in ${service.get('env')} mode.`);
--- server/service.js
+++ server/service.js
@@ -2,5 +2,22 @@
 
 const express = require('express');
 const service = express();
+const request = require('superagent');
+
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const GEOCODE_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+
+service.get('/service/:location', (req, res, next) => {
+    let locationParam = req.params.location;
+    let geocodeUrl = GEOCODE_API_URL + locationParam + `&key=${GEOCODE_API_KEY}`;
+
+    request.get(geocodeUrl, (err, response) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.json(response.body.results[0].geometry.location);
+    });
+});
 
 module.exports = service;
