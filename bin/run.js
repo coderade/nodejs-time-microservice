c048492cc07acd54139e88b8f5fbafe193a6dfc4
Valdeci Gomes <coderade@outlook.com>
2017-12-13 21:14:20 -0200

    Update bot connection log message

--- bin/run.js
+++ bin/run.js
@@ -11,7 +11,7 @@ server.on('listening', function () {
     const announce = () => {
         request.put(`http://127.0.0.1:3000/service/time/${server.address().port}`, (err, res) => {
             if (err) {
-                console.log('Error connecting to Siri');
+                console.log('Error connecting to Coderade bot.');
                 console.log(err);
                 return;
             }
--- server/service.js
+++ server/service.js
@@ -10,7 +10,7 @@ const GEOCODE_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 const TIMEZONE_API_KEY = process.env.TIMEZONE_API_KEY;
 const TIMEZONE_API_URL = 'https://maps.googleapis.com/maps/api/timezone/json';
 
-service.get('/service/:location', (req, res, next) => {
+service.get('/service/:location', (req, res) => {
     let locationParam = req.params.location;
 
     request
@@ -43,7 +43,7 @@ service.get('/service/:location', (req, res, next) => {
 
                     const result = response.body;
                     const timeString = moment.unix(timestamp + result.dstOffset + result.rawOffset)
-                        .utc().format('dddd, MMMM Do YYYY, h:mm:ss a')
+                        .utc().format('dddd, MMMM Do YYYY, h:mm:ss a');
 
                     res.json({result: timeString});
                 });
