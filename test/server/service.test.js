33c22c901f079b1a846220441af3964312194611
Valdeci Gomes <coderade@outlook.com>
2018-01-10 23:26:57 -0200

    Add some infrastructure tests

---
+++ test/server/service.test.js
@@ -0,0 +1,28 @@
+const should = require('should');
+const request = require('supertest');
+const service = require('../../server/service');
+
+describe('The express service', () => {
+    describe('GET /foo', () => {
+        it('should return HTTP 404', (done) => {
+            request(service)
+                .get('/foo')
+                .expect(404, done);
+        });
+    });
+});
+
+describe('PUT /service/:location', () => {
+    it('should return HTTP 200 with and a reply with a valid result', (done) => {
+        request(service)
+            .get('/service/Curitiba')
+            .expect(200)
+            .end((err, res) => {
+                if(err)
+                    return done(err);
+
+                should.exist(res.body.result);
+                return done();
+            });
+    });
+});
